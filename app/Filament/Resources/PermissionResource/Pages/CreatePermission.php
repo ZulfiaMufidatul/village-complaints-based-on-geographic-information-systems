651379492dd165e003fd4c8991568e76380bd68c
Zulfia Mufidatul <zulfiamufidatul@gmail.com>
2025-05-28 15:25:19 +0700

    add : function redirect ke index

--- app/Filament/Resources/PermissionResource/Pages/CreatePermission.php
+++ app/Filament/Resources/PermissionResource/Pages/CreatePermission.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePermission extends CreateRecord
 {
     protected static string $resource = PermissionResource::class;
+
+    public function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
