e5374459c610d4a57673c12be9f25ceffacf63fe
Zulfia Mufidatul <zulfiamufidatul@gmail.com>
2025-05-29 09:34:25 +0700

    ad : redirect to index

--- app/Filament/Resources/HamletResource/Pages/CreateHamlet.php
+++ app/Filament/Resources/HamletResource/Pages/CreateHamlet.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateHamlet extends CreateRecord
 {
     protected static string $resource = HamletResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
