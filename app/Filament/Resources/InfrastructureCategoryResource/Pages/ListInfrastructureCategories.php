19f24de11e8d2a27a92ec2d65585d66b94d3971d
Zulfia Mufidatul <zulfiamufidatul@gmail.com>
2025-07-03 13:57:34 +0700

    add : label tambah data

--- app/Filament/Resources/InfrastructureCategoryResource/Pages/ListInfrastructureCategories.php
+++ app/Filament/Resources/InfrastructureCategoryResource/Pages/ListInfrastructureCategories.php
@@ -13,7 +13,7 @@ class ListInfrastructureCategories extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->label('Tambah Kategori Infrastruktur'),
         ];
     }
 }
--- app/Filament/Resources/RWResource/Pages/ListRWS.php
+++ app/Filament/Resources/RWResource/Pages/ListRWS.php
@@ -13,7 +13,7 @@ class ListRWS extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->label('Tambah RW'),
         ];
     }
 }
