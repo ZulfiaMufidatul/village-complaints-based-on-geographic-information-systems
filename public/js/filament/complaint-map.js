df300e3490d48a88e8993783c79cf0d748bb3a4a
Zulfia Mufidatul <zulfiamufidatul@gmail.com>
2025-07-16 20:24:34 +0700

    add : daterange filter by malzeriey init

---
+++ public/js/filament/complaint-map.js
@@ -0,0 +1,14 @@
+// import L from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const map = L.map('complaint-map').setView([-6.9, 107.6], 13);
+
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         maxZoom: 19,
+//         attribution: '© OpenStreetMap'
+//     }).addTo(map);
+
+//     L.marker([-6.9, 107.6]).addTo(map)
+//         .bindPopup('Contoh Aduan Infrastruktur');
+// });
