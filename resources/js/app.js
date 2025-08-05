ff9ef5452c03511e7aca6f0ddc7b666ccec609f2
haafiidzh <hafid.kusuma45@gmail.com>
2025-08-05 20:52:18 +0700

    add : new asset

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,2 @@
-import Alpine from "alpinejs";
 import "./bootstrap";
 import "./complaintmap";
-import AOS from "aos";
-import 'aos/dist/aos.css';
-
-window.AOS = AOS;
-window.Alpine = Alpine;
-
-Alpine.start();
-AOS.init({
-    duration: 800,
-    easing: 'ease-in-out',
-    once: true,
-    mirror: false
-});
---
+++ resources/js/front.js
@@ -0,0 +1,14 @@
+import Alpine from "alpinejs";
+import AOS from "aos";
+import 'aos/dist/aos.css';
+
+window.AOS = AOS;
+window.Alpine = Alpine;
+
+Alpine.start();
+AOS.init({
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+});
